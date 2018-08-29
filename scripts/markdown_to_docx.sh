eae7e1bbf93b5e6c17f520a1bcf940efc42ef8df
owlman <jie.owl2008@gmail.com>
2018-08-29 19:15:45 +0800

    第二章docx版发布

---
+++ scripts/markdown_to_docx.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+# fileName : markdown_to_docx.sh
+# author   : lingjie
+# time     : 2018-08-29
+
+if [ $# != 1 ] || [ ${1##*.} != "md" ] ; then
+    echo "Usage: markdown_to_docx.sh <filename>.md"
+    exit
+fi
+
+pwd=$(dirname $0)
+fileName=${1##*/}
+srcFile="${pwd}/../translation/${fileName:0:-3}.md"
+objFile="${pwd}/../docx/${fileName:0:-3}.docx"
+if [ -f $srcFile ] ; then
+    if [ -f $objFile ] ; then
+        echo "目标文件已经存在"
+    else
+        pandoc -f markdown+tex_math_dollars -t docx $srcFile -o $objFile --latex-engine=xelatex
+    fi
+else
+   echo "源文件不存在"
+fi
